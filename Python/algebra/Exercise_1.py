22047cfa656795a7b9ff8694f2f16c348a806ee6
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-19 10:16:51 -0500

    Create Exercise_1.py

---
+++ Python/algebra/Exercise_1.py
@@ -0,0 +1,13 @@
+# Put a zero in for the unknown value
+n1 = 1 
+d1 = 2
+n2 = 4
+d2 = 0
+
+if n2 == 0:
+  answer = d2*n1/d1
+  print("n2 = ", answer)
+
+elif d2 == 0:
+  answer = n2*d1/d1
+  print("d2 = ", answer)
