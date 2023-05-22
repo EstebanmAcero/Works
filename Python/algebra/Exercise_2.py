01f01b4c5c4e8015642f985bf108045aeedf4b9f
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-21 20:04:32 -0500

    Create Exercise_2.py

---
+++ Python/algebra/Exercise_2.py
@@ -0,0 +1,12 @@
+import sympy 
+from sympy import symbols 
+from sympy.solvers import solve 
+
+x = symbols('x') 
+
+# Put the equation here
+eq = 2*x - 4
+
+print("x = ", solve(eq,x))
+
+# This program is for a equation equal to zero
