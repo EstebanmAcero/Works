4969ec6273c4d4074f41e5854ee963ee9c669745
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-22 14:52:52 -0500

    Create lists.py

---
+++ Python/Structures/lists.py
@@ -0,0 +1,22 @@
+# list 
+friends = [ 'Josehp', 'Glen', 'sail']
+carryon = [ 'socks', 'shirt', 'perfume']
+
+for i in [ 5,4,3,2,1] :
+	print(i)
+print('Blasstoff')
+
+z = [ 'Josehp', 'Glen', 'sail']
+for x in z:
+	print('Happy new year:',x)
+print('Done!')
+
+friends = [ 'Josehp', 'Glen', 'sail']
+print(friends[1])
+
+print(len(friends))
+
+# ranfe funtions returns a list of numbers
+#that range from zero to one less than the paramenter
+
+print (range(len(friends))
