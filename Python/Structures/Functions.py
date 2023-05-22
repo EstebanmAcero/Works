1ada057381750db6b7fb6618052bfafa527a99dc
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-21 23:37:31 -0500

    Update Functions.py

--- Python/Structures/Functions.py
+++ Python/Structures/Functions.py
@@ -8,5 +8,18 @@ i = 0
 type ( i )
 
 yu = max("Heloo buenoo")# print b because it's the maximun large value
-
 uy = min("Heloo bue") # print space because it's the minimiun value
+
+def greet (lang):
+	if lang == 'es'
+		print('Hola')
+
+	elif lang == 'fr'
+		print('Bonjour')
+
+	else:
+		print('Hello')
+		
+greet(es)
+greet(fr)
+greet()
