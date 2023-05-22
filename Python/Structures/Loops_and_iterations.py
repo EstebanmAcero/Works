0bd6ee7574f3b52a1244a0760020deea1241a207
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-21 23:58:34 -0500

    Update Loops_and_iterations.py

--- Python/Structures/Loops_and_iterations.py
+++ Python/Structures/Loops_and_iterations.py
@@ -1,1 +1,17 @@
 
+n = 7
+while n > 0 :
+	print(n)
+	n = n - 1
+print("Blasstoff")
+print(n)
+
+while True:
+	line = input('>')
+	if line [0] == '#'	:
+		continue	
+	if line == 'done'	:
+		break
+	print(line)
+
+print("Done!")
