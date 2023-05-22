479c7a6200a4b6c459593caead4d7371d215329a
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-22 00:11:04 -0500

    Update Loops_and_iterations.py

--- Python/Structures/Loops_and_iterations.py
+++ Python/Structures/Loops_and_iterations.py
@@ -20,10 +20,15 @@ print("Done!")
 for ii in [ 5,4,3,2,1]
 	print(ii)
 print ( 'Game over')
-#out 
-#5
-#4
-#3
-#2
-#1
+#out #5 #4#3#2#1
 #Game over
+
+#Finding the largest value
+largest_so_far = -1
+print("Before", largest_so_far)
+for the_num in [ 9, 41, 12, 3, 74, 15 ]:
+	if the_num > largest_so_far :
+		largest_so_far = the_num
+	print(largest_so_far, the_num)
+
+print('After',largest_so_far)
