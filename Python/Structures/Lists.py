1230cb2ab6005398e38240b9981aff6c03fc7eb0
EstebanmAcero <134076660+EstebanmAcero@users.noreply.github.com>
2023-05-22 15:02:58 -0500

    Update Lists.py

--- Python/Structures/Lists.py
+++ Python/Structures/Lists.py
@@ -20,3 +20,43 @@ print(len(friends))
 #that range from zero to one less than the paramenter
 
 print (range(len(friends))
+
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print(c)
+a[1:2]
+a[:1]
+a[1:]
+a[:]
+
+x = list()
+type (x)
+dir (x) # that indicades all that can do or th methods
+
+stuff = list()
+stuff.append('book')
+stuff.append('00')
+# append adds elements
+some = [1,5,345,62,3,9]
+9 in some
+#true
+
+friends = [ 'Josehp', 'Glen', 'sail']
+friends.sort()
+print(friends)
+
+
+# function apply to list
+# len , max, min, sum
+
+
+numlist = list()
+while True:
+	inp = input ('Enter a number')
+	if inp == 'Done': break
+	value = float (inp)
+	numlist.append(value)
+
+average = sum(numlist)/len(numlist)
+print('Average: ', average)
